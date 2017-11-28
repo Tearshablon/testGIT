c3c4332dbdd276bf0551b1365fce8334c8844b2c
Dmitry Shablov <shablov.d@gmail.com>
2017-11-28 08:11:04 +0300

    добавил сложение

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int a = 1;
+        int b = 2;
+        System.out.println(a+b);
     }
 }
